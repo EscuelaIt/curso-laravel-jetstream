57783fdb1a7d940a6f161a13c6bc3c5b11cd816f
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2022-04-07 13:51:31 -0500

    Clase 2: 2.	Yendo a profundidad con Tailwind CSS

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,63 @@
+<table class="table-fixed">
+    <thead class="border-y border-gray-500 border-y-2">
+        <tr>
+            <th class="w-2/12">
+                Title
+            </th>
+            <th class="w-3/12">
+                Description
+            </th>
+            <th class="w-3/12">
+                Preview
+            </th>
+            <th class="w-1/12">
+                Created at
+            </th>
+          <th class="w-3/12"></th>
+        </tr>
+    </thead>
+  <tbody>
+    <tr>
+        <td class="px-2">Malcolm Lockyer</td>
+        <td class="px-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+        <td class="px-2"><img src="#" alt="Title"></td>
+        <td class="px-2">5 hours ago</td>
+        <td class="flex px-2 flex-row">
+            <x-jet-button class="mr-2 bg-blue-300">
+                Edit
+            </x-jet-button>
+            <x-jet-button class="mr-2 bg-red-300">
+                Remove
+            </x-jet-button>
+        </td>
+    </tr>
+    <tr>
+        <td class="px-2">Lorem ipsum, dolor sit amet, consectetur adipisicing.</td>
+        <td class="px-2">Optio sed at similique fugiat in. Deserunt eveniet nostrum cupiditate expedita aliquam voluptate.</td>
+        <td class="px-2"><img src="#" alt="Title"></td>
+        <td class="px-2">10 hours ago</td>
+        <td class="flex px-2 flex-row">
+            <x-jet-button class="mr-2 bg-blue-300">
+                Edit
+            </x-jet-button>
+            <x-jet-button class="mr-2 bg-red-300">
+                Remove
+            </x-jet-button>
+        </td>
+    </tr>
+    <tr>
+        <td class="px-2">Dolor sit amet, consectetur adipisicing.</td>
+        <td class="px-2">Similique fugiat in. Deserunt eveniet nostrum cupiditate expedita aliquam voluptate.</td>
+        <td class="px-2"><img src="#" alt="Title"></td>
+        <td class="px-2">12 hours ago</td>
+        <td class="flex px-2 flex-row">
+            <x-jet-button class="mr-2 bg-blue-300">
+                Edit
+            </x-jet-button>
+            <x-jet-button class="mr-2 bg-red-300">
+                Remove
+            </x-jet-button>
+        </td>
+    </tr>
+  </tbody>
+</table>
--- resources/views/livewire/resources.blade.php
+++ resources/views/livewire/resources.blade.php
@@ -1,26 +1,19 @@
 <div>
-    <h1>Hello</h1>
-
-    <x-jet-button class="bg-blue-500">
-        Exit
-    </x-jet-button>
-
-    <x-jet-dialog-modal>
-        <x-slot name="title">
-            Hola MUndo
-        </x-slot>
-
-        <x-slot name="content">
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo minima deserunt natus optio quisquam doloremque nobis ullam aspernatur quidem quasi praesentium hic eos adipisci, voluptatem magnam exercitationem a corporis repudiandae.
-            </p>
-        </x-slot>
-
-        <x-slot name="footer">
-            <ul>
-                <li>FB</li>
-                <li>TW</li>
-            </ul>
-        </x-slot>
-    </x-jet-dialog-modal>
+    <div class="flex justify-center">
+        <h1 class="
+            font-bold
+            text-2xl
+            text-center
+            mt-5
+            mb-24
+            border-b
+            border-b-2
+            border-gray-600"
+        >
+            File Media Manager with other things
+        </h1>
+    </div>
+    <div class="p-5">
+        <x-table></x-table>
+    </div>
 </div>
